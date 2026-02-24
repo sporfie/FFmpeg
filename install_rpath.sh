badfdfe1b01fcca48192d8005d73263bc42fd97f
Guy Moreillon <guy.moreillon@brozermo.com>
2026-02-24 10:46:47 +0100

    Added macos rpath support

---
+++ install_rpath.sh
@@ -0,0 +1,3 @@
+install_name_tool -add_rpath /usr/lib /Users/guy/Development/Sporfie/ffmpeg_sporfie/ffmpeg
+install_name_tool -add_rpath /usr/lib /Users/guy/Development/Sporfie/ffmpeg_sporfie/ffprobe
+
