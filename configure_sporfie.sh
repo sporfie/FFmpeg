20ec141b35803271f3aae0d2a276e104b3e853ff
Guy Moreillon <guy.moreillon@brozermo.com>
2026-08-17 13:42:26 +0200

    Update of configure_sporfie.sh

--- configure_sporfie.sh
+++ configure_sporfie.sh
@@ -1,1 +1,26 @@
-./configure --enable-libsrt --enable-libfreetype --enable-fontconfig --enable-gpl --enable-nonfree --enable-filter=drawtext --enable-libharfbuzz --disable-bzlib
+# 1. Force the compiler to look at Homebrew FIRST for headers
+export CPPFLAGS="-I/opt/homebrew/include -I/opt/homebrew/opt/freetype/include/freetype2 -I/opt/homebrew/opt/libpng/include/libpng16"
+
+# 2. Force the linker to look at Homebrew FIRST for libraries
+export LDFLAGS="-L/opt/homebrew/lib -L/opt/homebrew/opt/openssl@3/lib"
+
+# 3. Make sure pkg-config doesn't even look at the GStreamer folder
+export PKG_CONFIG_PATH="/opt/homebrew/lib/pkgconfig:/opt/homebrew/opt/openssl@3/lib/pkgconfig:/opt/homebrew/opt/libsrt/lib/pkgconfig"
+
+./configure \
+  --enable-libsrt \
+  --enable-libfreetype \
+  --enable-fontconfig \
+  --enable-gpl \
+  --enable-nonfree \
+  --enable-filter=drawtext \
+  --enable-libharfbuzz \
+  --disable-bzlib \
+  --enable-libx264 \
+  --enable-libx265 \
+  --enable-openssl \
+  --enable-videotoolbox \
+  --enable-audiotoolbox \
+  --extra-cflags="$CPPFLAGS" \
+  --extra-ldflags="$LDFLAGS"
+  
